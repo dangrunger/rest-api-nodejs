8111f0cbef788514a589003c75463aa72e369abb
Daniel Cunha de Oliveira <daniel.oliveira@coopersystem.com.br>
2022-08-26 10:58:20 -0300

    🚀 Sequelize Stuffs (new migration and model sett)

---
+++ db/migrations/20220825190538-create-tb_product.js
@@ -0,0 +1,42 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("tb_product", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      removed_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tb_product')
+  },
+};
